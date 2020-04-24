38a9dc831597bd7e4536f8a2d2be00ee2172633b
tarush-r <tarush.r@somaiya.edu>
2020-04-24 20:06:53 +0530

    added django_heroku

--- myproject/settings.py
+++ myproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -124,3 +125,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 STATIC_ROOT=os.path.join(BASE_DIR, 'assets')
+
+django_heroku.settings(locals())
