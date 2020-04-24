5ddcd2411786458f96f9904a4686b2063513ae5e
tarush-r <tarush.r@somaiya.edu>
2020-04-24 20:00:23 +0530

    updated settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*#2rqfaufm3xr&xyrxt$2!l&uaesvj_qw=b6%tej-!vj_1+&)s'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['trackatd.herokuapp.com']
 
 
 # Application definition
